65a8d11b9fd650fc11f3c63e231dabfec75762d0
Chaise Farrar <chaise.farrar@gmail.com>
2017-07-16 21:56:20 -0500

    Added text explaining program and pulled current count of cups

--- coffee-tracker.go
+++ coffee-tracker.go
@@ -21,14 +21,17 @@ func main() {
 
     fmt.Println("Connected to redis-server.")
 
+    fmt.Println("Welcome to my Coffee Counter. You can see how many times I've made coffee with the given brew type. Then you can increment one when you've made another cup")
   // Get list of Current coffee Items
     cl,err := client.LLen("coffees")// cl is a variable to store length of "coffees"
     // grab "coffees" list and store it in "Coffees" array
     Coffees := make([]string,int(cl))
     Coffees,err = client.LRange("coffees",0,-1)
     //print out each of the items
-    var n int64 
-    for n = 0; n <= cl; n++ {
+    var n int64
+    for n = 0; n < cl; n++ {
+        count,err = client.Get(Coffees[n])
+        option = string(n+1) + ". " + Coffees[n] + " : " + string(count) + " Cups"
         fmt.Println(Coffees[n])
     }
 
